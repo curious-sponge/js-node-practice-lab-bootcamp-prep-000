b2512a1b3202b1ac586a1fbb220016f6f234066e
sedat_3278@hotmail.com <sedat_3278@hotmail.com>
2018-05-01 18:43:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 var name = "Abraham"
 var height = "89"
 
+var age = 30
+
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(parseInt(height) + 10 )
+
+console.log("Age: ")
+console.log(age)
